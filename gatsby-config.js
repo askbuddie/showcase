9dc3ac49362cf01054e4ae8a2e26d6b04dfbe886
ashiishme <iamashiishy@gmail.com>
2021-10-03 17:50:31 +0545

    refactor: update project config & content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,33 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Ask Buddie Showcase",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Ask Buddie Showcase',
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-plugin-mdx",
+    'gatsby-plugin-mdx',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'project',
+        path: './src/content/',
+      },
+      __key: 'project',
     },
   ],
-};
+}
