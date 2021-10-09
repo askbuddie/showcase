c9e1e9166cb83fdca15f5de0a4da11339b6488ec
ashiishme <iamashiishy@gmail.com>
2021-10-09 08:28:15 +0545

    refactor: add brand color in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E53935',
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    'gatsby-plugin-postcss',
-  ],
-}
+  plugins: ['gatsby-plugin-postcss'],
+};
