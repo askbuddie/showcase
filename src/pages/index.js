493385052430635bc3eb03941957420854943571
ashiishme <iamashiishy@gmail.com>
2021-10-09 08:29:23 +0545

    refactor: update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
-import * as React from 'react'
-import { Projects } from '../templates/Projects'
+import * as React from 'react';
+import { Projects } from '../templates/Projects';
 
 // markup
 const IndexPage = () => {
   return (
-    <main>
+    <main className="container mx-auto">
+      <h1>Awesome Collection of projects from Buddies </h1>
       <Projects />
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
